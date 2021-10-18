b6df58f11c5b7027659f921f854545fc3c048d76
James Moore <james816am@gmail.com>
2021-10-18 14:52:51 -0400

    Submit button is functional, mapped out javascript

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const root = document.getElementById('root');
+const form = document.getElementById('inputForm')
 
 //event listener for submit button 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(document.getElementById('inputField').value);
+    const inputField = document.getElementById('inputField').value;
+    createMovie(inputField);
+})
+//create movie notecard - POST info to JSON
+function createMovie(input) {
 
-//create movie notecard
+}
 
 //edit feature 
 
 //true/false button to say whether or not it has been watched
+
+//render movie card text and buttons/dates (title, watched-true/false, added_at-moment, watched_at-moment)
+
+//
