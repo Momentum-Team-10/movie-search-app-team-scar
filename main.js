50de3986ee5e9f6c1ce5447d85124987f088d54f
Lila <djyasin.ly@gmail.com>
2021-10-18 14:33:40 -0400

    Built basic skeletion of html, started mapping the functions.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 const root = document.getElementById('root');
+
+//event listener for submit button 
+
+//create movie notecard
+
+//edit feature 
+
+//true/false button to say whether or not it has been watched
