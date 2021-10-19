c68d32e63a443412a838ffa785dcef65637a3287
James Moore <james816am@gmail.com>
2021-10-19 15:08:35 -0400

    Edit button in progress

--- main.js
+++ main.js
@@ -6,27 +6,27 @@ const movieList = document.getElementById("movieList");
 
 //event listener for submit button
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log(document.getElementById("inputField").value);
-  const inputField = document.getElementById("inputField").value;
-  createMovie(inputField);
-  form.reset();
+    e.preventDefault();
+    console.log(document.getElementById("inputField").value);
+    const inputField = document.getElementById("inputField").value;
+    createMovie(inputField);
+    form.reset();
 });
 //create movie notecard - POST info to JSON
 function createMovie(input) {
-  fetch(url, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      title: input,
-      added_at: moment().format(),
-    }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      renderMovieCard(data);
-    });
+    fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            title: input,
+            added_at: moment().format(),
+        }),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            renderMovieCard(data);
+        });
 }
 
 //edit feature
@@ -36,46 +36,62 @@ function createMovie(input) {
 //render movie card text and buttons/dates (title, watched-true/false, added_at-moment, watched_at-moment)
 
 function renderMovieCard(movieInfo) {
-  const movieItem = document.createElement("li");
-  movieItem.id = movieInfo.id;
-  renderCardContents(movieItem, movieInfo);
-  movieList.appendChild(movieItem);
-  console.log(movieInfo);
+    const movieItem = document.createElement("li");
+    movieItem.id = movieInfo.id;
+    renderCardContents(movieItem, movieInfo);
+    movieList.appendChild(movieItem);
+    console.log(movieInfo);
 }
 //radio button functionality pause, need to finish later
 function renderCardContents(li, cardText) {
-  li.innerHTML = `
+    li.innerHTML = `
     <p>${cardText.title}</p>
     <p>Added on ${moment(cardText.added_at).format("MMM DD, YYYY")}</p>
 <form> 
 <input type="radio" id="${cardText.title}">
 <label>  Click if Watched </label>
 </form>
-<i class="delete-button fas fa-trash-alt" ></i >
+<i class="deleteButton fas fa-trash-alt" ></i >
+<i class="editButton fas fa-edit" ></i>
     `;
 }
 
 movieList.addEventListener("click", (event) => {
-const movieCard = document.getElementById("inputField").value;
-if (event.target.classList.contains("delete-button")){deleteMovie(event.target)}
-}) 
-
+    const movieCard = document.getElementById("inputField").value;
+    if (event.target.classList.contains("deleteButton")) { deleteMovie(event.target) }
+    if (event.target.classList.contains("editButton")) { editMovie(event.target) }
+    
+})
 
 
 function listMovieCard() {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      for (let item of data) {
-        renderMovieCard(item);
-      }
-    });
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            for (let item of data) {
+                renderMovieCard(item);
+            }
+        });
 }
 
 function deleteMovie(movieCard) {
-  fetch(url + "/" + `${movieCard.parentElement.id}`, {
-    method: "DELETE",
-  }).then(() => movieCard.parentElement.remove());
+    fetch(url + "/" + `${movieCard.parentElement.id}`, {
+        method: "DELETE",
+    }).then(() => movieCard.parentElement.remove());
+}
+
+function editMovie(movieCard) {
+    fetch(url + "/" + `${movieCard.parentElement.id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            title: movieCard,
+        })
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            renderCardContents(movieCard.parentElement, data)
+        })
 }
 
 listMovieCard();
