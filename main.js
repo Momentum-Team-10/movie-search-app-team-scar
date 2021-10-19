68811468a7abe426e959c5a9ce409e60b49527e3
Lila <djyasin.ly@gmail.com>
2021-10-19 14:46:31 -0400

    got delete button functionality working.

--- main.js
+++ main.js
@@ -1,85 +1,83 @@
-const root = document.getElementById('root');
-const form = document.getElementById('inputForm');
+const root = document.getElementById("root");
+const form = document.getElementById("inputForm");
 const url = "http://localhost:3000/movies";
-const movieList = document.getElementById('movieList');
+const movieList = document.getElementById("movieList");
 
 
-//event listener for submit button 
+//event listener for submit button
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log(document.getElementById('inputField').value);
-    const inputField = document.getElementById('inputField').value;
-    createMovie(inputField);
-    form.reset();
-})
+  e.preventDefault();
+  console.log(document.getElementById("inputField").value);
+  const inputField = document.getElementById("inputField").value;
+  createMovie(inputField);
+  form.reset();
+});
 //create movie notecard - POST info to JSON
 function createMovie(input) {
-    fetch(url, {
-        method: "POST",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-        title: input,
-        added_at: moment().format()
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        renderMovieCard(data)
-    }
-    )
+  fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      title: input,
+      added_at: moment().format(),
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      renderMovieCard(data);
+    });
 }
 
-//edit feature 
+//edit feature
 
 //true/false button to say whether or not it has been watched
 
 //render movie card text and buttons/dates (title, watched-true/false, added_at-moment, watched_at-moment)
+
 function renderMovieCard(movieInfo) {
-const movieItem = document.createElement("li");
-movieItem.id = movieInfo.id;
-renderCardContents(movieItem,movieInfo);
-movieList.appendChild(movieItem);
-console.log(movieInfo)
+  const movieItem = document.createElement("li");
+  movieItem.id = movieInfo.id;
+  renderCardContents(movieItem, movieInfo);
+  movieList.appendChild(movieItem);
+  console.log(movieInfo);
 }
+//radio button functionality pause, need to finish later
 function renderCardContents(li, cardText) {
-    li.innerHTML = `
+  li.innerHTML = `
     <p>${cardText.title}</p>
-    <p>Added on ${moment(cardText.added_at).format('MMM DD, YYYY')}</p>
-    `
+    <p>Added on ${moment(cardText.added_at).format("MMM DD, YYYY")}</p>
+<form> 
+<input type="radio" id="${cardText.title}">
+<label>  Click if Watched </label>
+</form>
+<i class="delete-button fas fa-trash-alt" ></i >
+    `;
 }
+
+movieList.addEventListener("click", (event) => {
+const movieCard = document.getElementById("inputField").value;
+if (event.target.classList.contains("delete-button")){deleteMovie(event.target)}
+}) 
+
+
 
 function listMovieCard() {
-    fetch(url)
-    .then(response => response.json())
-    .then(data => { 
-       for(let item of data){
-        renderMovieCard(item)
-       }     
-    })
-
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      for (let item of data) {
+        renderMovieCard(item);
+      }
+    });
 }
 
-function deleteMovie(movieCard){
-    fetch(url+ "/" + `${movieCard.parentElement.id}`, {
-        method: "DELETE",
-
-    })
-
-.then (() => movieCard.parentElement.remove())
-    
-
-
+function deleteMovie(movieCard) {
+  fetch(url + "/" + `${movieCard.parentElement.id}`, {
+    method: "DELETE",
+  }).then(() => movieCard.parentElement.remove());
 }
-
-
-
-
-
-
 
 listMovieCard();
 
-
-
-//movie card button features 
+//movie card button features
