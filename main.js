56b757af48895c125db15618e644b71db001bc38
Trey Williams <fly.tech157@gmail.com>
2021-10-19 11:29:36 -0400

    created a delete function and save fucntion

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function createMovie(input) {
     .then(response => response.json())
     .then(data => {
         console.log(data)
-        renderMoviecard(data)
+        renderMovieCard(data)
     }
     )
 }
@@ -35,7 +35,7 @@ function createMovie(input) {
 //true/false button to say whether or not it has been watched
 
 //render movie card text and buttons/dates (title, watched-true/false, added_at-moment, watched_at-moment)
-function renderMoviecard(movieInfo) {
+function renderMovieCard(movieInfo) {
 const movieItem = document.createElement("li");
 movieItem.id = movieInfo.id;
 renderCardContents(movieItem,movieInfo);
@@ -45,9 +45,41 @@ console.log(movieInfo)
 function renderCardContents(li, cardText) {
     li.innerHTML = `
     <p>${cardText.title}</p>
-    <p>${moment(cardText.added_at).format('MMM DD, YYYY')}</p>
+    <p>Added on ${moment(cardText.added_at).format('MMM DD, YYYY')}</p>
     `
 }
+
+function listMovieCard() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => { 
+       for(let item of data){
+        renderMovieCard(item)
+       }     
+    })
+
+}
+
+function deleteMovie(movieCard){
+    fetch(url+ "/" + `${movieCard.parentElement.id}`, {
+        method: "DELETE",
+
+    })
+
+.then (() => movieCard.parentElement.remove())
+    
+
+
+}
+
+
+
+
+
+
+
+listMovieCard();
+
 
 
 //movie card button features 
